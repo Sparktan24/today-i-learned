17c56d68a4c85a41240436827bc3bba454631a95
Neto Molina <netod90@gmail.com>
2023-03-02 23:46:13 -0700

    Update Header component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,17 @@
 import "./style.css";
+import Header from "./components/Header";
 import NewFactForm from "./components/NewFactForm";
 import { CategoryFilter } from "./components/CategoryFilter";
 import FactsList from "./components/FactsList";
 import { useState } from "react";
 
 function App() {
-  // 1. DEFINE STATE VARIABLE
   const [showForm, setShowForm] = useState(false);
 
   return (
     <>
-      {/* HEADER */}
-      <header className="header">
-        <div className="logo">
-          <img
-            src="logo.png"
-            height="68"
-            width="68"
-            alt="Today I Learned Logo"
-          />
-          <h1>Today I Learned</h1>
-        </div>
+      <Header showForm={showForm} setShowForm={setShowForm} />
 
-        <button
-          className="btn btn-large btn-open"
-          //3. UPDATE STATE VARIABLE
-          onClick={() => setShowForm((state) => !state)}
-        >
-          Share a fact
-        </button>
-      </header>
-
-      {/* 2. USE STATE VARIABLE */}
       {showForm ? <NewFactForm /> : null}
 
       <main className="main">
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+// CurrentState, Function to change state
+function Header({ showForm, setShowForm }) {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
+        <h1>Today I Learned</h1>
+      </div>
+
+      <button
+        className="btn btn-large btn-open"
+        onClick={() => setShowForm((state) => !state)}
+      >
+        {showForm ? "Close" : "Share a fact"}
+      </button>
+    </header>
+  );
+}
+
+export default Header;
