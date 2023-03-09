f78afeb20ed908d9c4368c53e3ae93c35b2e129c
Neto Molina <netod90@gmail.com>
2023-03-09 00:54:58 -0700

    Add new fact functionality

--- src/components/NewFactForm.js
+++ src/components/NewFactForm.js
@@ -1,14 +1,50 @@
 import { useState } from "react";
 import { CATEGORIES } from "./CategoryFilter";
 
-function NewFactForm() {
+function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText] = useState("");
-  const [source, setSource] = useState("");
+  const [source, setSource] = useState("http://example.com");
   const [category, setCategory] = useState("");
 
+  function isValidHttpUrl(string) {
+    let url;
+    try {
+      url = new URL(string);
+    } catch (_) {
+      return false;
+    }
+    return url.protocol === "http:" || url.protocol === "https:";
+  }
+
   function handleSubmit(e) {
+    // 1.Prevent the browser reload
     e.preventDefault();
     console.log(text, source, category);
+    //  2. Check if data is valid. If so, create a new fact
+    if (text && isValidHttpUrl(source) && category && text.length <= 200) {
+      //  3. Create a new fact object
+      const newFact = {
+        id: Math.round(Math.random() * 100000000),
+        text,
+        source,
+        category,
+        votesInteresting: 0,
+        votesMindblowing: 0,
+        votesFalse: 0,
+        createdIn: new Date().getFullYear(),
+      };
+
+      //  4. Add the new fact object to the UI: add the fact to state
+      //  Take out all elements of previous facts array and place them in the new array
+      setFacts((facts) => [newFact, ...facts]);
+
+      //  5. Reset input fields (Already resets as default beheavior)
+      /*       setText("");
+      setSource("");
+      setCategory(""); */
+      //  6. Close the form
+      setShowForm(false);
+    }
   }
 
   return (
